fca342a25947fb57a79f10f80f22b86d483e45b3
Winston Smith <59091490+github4touchdouble@users.noreply.github.com>
2024-04-19 15:54:08 +0200

    added smss class

---
+++ src/Optimal.java
@@ -0,0 +1,5 @@
+public class Optimal extends SMSS_Problem {
+    public Optimal(int[] sequence) {
+        super(sequence);
+    }
+}
--- src/SMSS_Problem.java
+++ src/SMSS_Problem.java
@@ -1,10 +1,8 @@
-public class SMSS_Problam {
+public class SMSS_Problem {
     public int[] sequence;
     public long max_sum;
-    public SMSS_Problam(int[] sequence) {
+    public SMSS_Problem(int[] sequence) {
         this.sequence = sequence;
         this.max_sum = 0;
     }
-
-
 }
