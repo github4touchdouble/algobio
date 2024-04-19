62f746485eb8f707243d5f1820fd05e9e59cb8ca
Winston Smith <59091490+github4touchdouble@users.noreply.github.com>
2024-04-19 16:31:25 +0200

    updated optimal algorithm

--- src/SMSS_Problem.java
+++ src/SMSS_Problem.java
@@ -1,18 +1,19 @@
+import java.util.List;
+
 public class SMSS_Problem {
 
-    public static int Optimal(int[] sequence) {
+    public static int Optimal(List<Integer> sequence) {
         int max = 0;
         int l = 1;
         int r = 0;
         int rmax = 0;
         int rstart = 1;
-        int n = sequence.length;
-
+        int n = sequence.size();
         for (int i = 1; i < n; i++) {
             if (rmax > 0) {
-                rmax = rmax + sequence[i];
+                rmax = rmax + sequence.get(i);
             } else {
-                rmax = sequence[i];
+                rmax = sequence.get(i);
                 rstart = i;
             }
             if (rmax > max) {
