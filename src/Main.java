0031dcc30db9f295528843755ec7a9e2d27f6309
Malte Weyrich <malte.weyrich@hotmail.de>
2024-04-19 16:18:11 +0200

    working on argparse

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,18 @@ import net.sourceforge.argparse4j.inf.Namespace;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ArgumentParserException {
         System.out.println("Hello");
+        ArgumentParser parser = ArgumentParsers.newFor("GOR").build().defaultHelp(true).description("Predict Secondary Structure");
+        parser.addArgument("--v")
+                .metavar("N")
+                .type(Integer.class)
+                .nargs("+")
+                .help("input vector");
+
+        Namespace ns = parser.parseArgs(args);
+        String vec = ns.getString("--v");
+        System.out.println(vec);
+
     }
 }
