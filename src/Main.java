fa7097974e79950ebb79a7daa018a9c44849dacb
Malte Weyrich <malte.weyrich@hotmail.de>
2024-04-19 16:34:04 +0200

    fixing nargs

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,21 @@ import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
 
+import javax.naming.Name;
+
 
 public class Main {
     public static void main(String[] args) throws ArgumentParserException {
-        System.out.println("Hello");
-        ArgumentParser parser = ArgumentParsers.newFor("GOR").build().defaultHelp(true).description("Predict Secondary Structure");
-        parser.addArgument("--v")
+        ArgumentParser parser = ArgumentParsers.newFor("MSS").build().defaultHelp(true).description("Predict Secondary Structure"); parser.addArgument("--v")
                 .metavar("N")
                 .type(Integer.class)
                 .nargs("+")
                 .help("input vector");
 
         Namespace ns = parser.parseArgs(args);
-        String vec = ns.getString("--v");
-        System.out.println(vec);
+        // get vals to list
+        java.util.List<Integer> vec = ns.getList("v");
 
+        SMSS_Problem.Optimal(vec);
     }
 }
