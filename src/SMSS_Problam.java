8f8008cc7866322e601e05cc9dbd942308b1e016
Winston Smith <59091490+github4touchdouble@users.noreply.github.com>
2024-04-19 15:50:29 +0200

    added smss class

---
+++ src/SMSS_Problam.java
@@ -0,0 +1,10 @@
+public class SMSS_Problam {
+    public int[] sequence;
+    public long max_sum;
+    public SMSS_Problam(int[] sequence) {
+        this.sequence = sequence;
+        this.max_sum = 0;
+    }
+
+
+}
