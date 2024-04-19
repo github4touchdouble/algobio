55b94ef01558d5e2f1c18bd09fdbd4f815e911b1
Malte Weyrich <malte.weyrich@hotmail.de>
2024-04-19 18:08:48 +0200

    refactoring main
    Now utilizing switch case

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,13 @@ import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
 
+import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) throws ArgumentParserException {
-        ArgumentParser parser = ArgumentParsers.newFor("MSS").build().defaultHelp(true).description("Predict Secondary Structure"); parser.addArgument("--v")
+        ArgumentParser parser = ArgumentParsers.newFor("MSS").build().defaultHelp(true).description("Calculate maximum scoring subsequence for given input vector --v");
+        parser.addArgument("--v")
                 .metavar("N")
                 .type(Integer.class)
                 .nargs("+")
@@ -17,20 +20,62 @@ public class Main {
         // get vec
         java.util.List<Integer> vec = ns.getList("v");
 
-        int[] resOptimal = SMSS_Problem.optimal(vec);
-        printRes(resOptimal);
+        benchmarkCode("naive", vec);
+        benchmarkCode("recursive", vec);
+        benchmarkCode("dynamic", vec);
+        benchmarkCode("optimal", vec);
 
-        int[] resNaive = SMSS_Problem.naive(vec);
-        printRes(resOptimal);
-
-        int[] resRec = SMSS_Problem.rec(vec);
-        printRes(resRec);
-
-        int[] resDynamic = SMSS_Problem.dynamic(vec);
-        printRes(resDynamic);
     }
 
-    public static void printRes(int[] res) {
+    public static void printRes(int[] res, long time) {
         System.out.println("[" + res[0] + "," + res[1] +"] mit score " + res[2]);
+        System.out.println(time + " µs\n");
+    }
+
+    public static void benchmarkCode(String type, List<Integer> vec) {
+        long startTime = 0;
+        long endTime = 0;
+        long elapsedTimeMicros = 0;
+
+        switch (type) {
+            case ("optimal"):
+                startTime = System.nanoTime();
+                int[] resOptimal = SMSS_Problem.optimal(vec);
+                endTime = System.nanoTime();
+                elapsedTimeMicros = (endTime - startTime) / 1000;
+                System.out.println("Optimal:");
+                printRes(resOptimal, elapsedTimeMicros);
+                break;
+
+            case("naive"):
+                startTime = System.nanoTime();
+                int[] resNai = SMSS_Problem.naive(vec);
+                endTime = System.nanoTime();
+                elapsedTimeMicros = (endTime - startTime) / 1000;
+                System.out.println("Naive:");
+                printRes(resNai, elapsedTimeMicros);
+                break;
+
+            case ("recursive"):
+                startTime = System.nanoTime();
+                int[] resRec = SMSS_Problem.rec(vec);
+                endTime = System.nanoTime();
+                elapsedTimeMicros = (endTime - startTime) / 1000;
+                System.out.println("Recursive:");
+                printRes(resRec, elapsedTimeMicros);
+                break;
+
+            case ("dynamic"):
+                startTime = System.nanoTime();
+                int[] resDyn = SMSS_Problem.dynamic(vec);
+                endTime = System.nanoTime();
+                elapsedTimeMicros = (endTime - startTime) / 1000;
+                System.out.println("Dynamic Programming:");
+                printRes(resDyn, elapsedTimeMicros);
+                break;
+
+
+
+       }
     }
 }
