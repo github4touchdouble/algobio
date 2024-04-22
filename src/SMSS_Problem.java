860029e03f3016e335823b799b36eb8e69361aa1
Winston Smith <59091490+github4touchdouble@users.noreply.github.com>
2024-04-22 02:11:21 +0200

    updated d&c

--- src/SMSS_Problem.java
+++ src/SMSS_Problem.java
@@ -30,14 +30,57 @@ public class SMSS_Problem {
         return new int[]{l, r, max};
     }
 
-    public static int[] divide_and_conquer (List<Integer> sequence) {
-        return new int[]{0, 0, 0};
+    public static int[] divide_and_conquer(List<Integer> sequence) {
+        return rec_divide_and_conquer(sequence, 0, sequence.size() - 1);
     }
+
+    private static int[] rec_divide_and_conquer(List<Integer> sequence, int i, int j) {
+        if (i == j) {
+            if (sequence.get(i) > 0) {
+                return new int[]{i, i, sequence.get(i)};
+            } else {
+                return new int[]{i, i - 1, 0};
+            }
+        }
+        int m = (i + j - 1) / 2;
+        int[] t1 = rec_divide_and_conquer(sequence, i, m);
+        int[] t2 = rec_divide_and_conquer(sequence, m + 1, j);
+        int[] t3 = new int[]{0, 0, 0};
+        for (int k = i; k <= m; k++) {
+            int sigma = sigRec(k, m, sequence);
+            if (sigma > t3[0]) {
+                t3[0] = sigma;
+            }
+        }
+        for (int k = m + 1; k <= j; k++) {
+            int sigma = sigRec(m + 1, k, sequence);
+            if (sigma < t3[1]) {
+                t3[1] = sigma;
+            }
+        }
+        t3[2] = sig(t3[0], t3[1], sequence);
+
+        if (t1[2] > t2[2] && t1[2] > t3[2]) {
+            return t1;
+        } else if (t2[2] > t1[2] && t2[2] > t3[2]) {
+            return t2;
+        } else {
+            return t3;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] test = new int[]{-5, 2, 4, -4, 5};
+        //int[] optimal = optimal(new ArrayList<>(List.of(-5, 2, 4, -4, 5)));
+        int[] optimal = divide_and_conquer(new ArrayList<>(List.of(-5, 2, 4, -4, 5)));
+        System.out.println("Optimal: [" + optimal[0] + "," + optimal[1] + "] mit score " + optimal[2]);
+    }
+
 
     public static int[] naive(List<Integer> sequence) {
         // init
         // [l, r, max]
-        int[] res = new int[]{1,0,0};
+        int[] res = new int[]{1, 0, 0};
         int n = sequence.size();
 
         for (int i = 0; i < n; i++) {
@@ -58,28 +101,28 @@ public class SMSS_Problem {
     public static int[] rec(List<Integer> sequence) {
         // init
         // [l, r, max]
-        int[] res = new int[]{1,0,0};
+        int[] res = new int[]{1, 0, 0};
         int n = sequence.size();
 
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
-               int s = sigRec(i, j, sequence);
+                int s = sigRec(i, j, sequence);
 
-               if (s >= res[2]) {
-                   res[0] = i; // l
-                   res[1] = j; // r
-                   res[2] = s; // update max score
-               }
+                if (s >= res[2]) {
+                    res[0] = i; // l
+                    res[1] = j; // r
+                    res[2] = s; // update max score
+                }
             }
         }
 
-        return  res;
+        return res;
     }
 
-    public static int[] dynamic(List<Integer> sequence){
+    public static int[] dynamic(List<Integer> sequence) {
         // init
         // [l, r, max]
-        int[] res = new int[]{1,0,0};
+        int[] res = new int[]{1, 0, 0};
         int n = sequence.size();
 
         int[][] S = new int[n][n];
@@ -122,9 +165,9 @@ public class SMSS_Problem {
                 l = rstart;
                 r = i;
                 scores.add(new ArrayList<>());
-                scores.get(scores.size()-1).add(new int[] {l, r, max});
+                scores.get(scores.size() - 1).add(new int[]{l, r, max});
             } else if (rmax == max) {
-                scores.get(scores.size()-1).add(new int[] {rstart, i, max});
+                scores.get(scores.size() - 1).add(new int[]{rstart, i, max});
             }
         }
         return scores.get(scores.size() - 1);
@@ -132,8 +175,8 @@ public class SMSS_Problem {
 
     public static int sig(int i, int j, List<Integer> sequence) {
         int sequenceScore = 0;
-        for (int k = i; k <= j ; k++) {
-           sequenceScore += sequence.get(k);
+        for (int k = i; k <= j; k++) {
+            sequenceScore += sequence.get(k);
         }
         return sequenceScore;
     }
@@ -146,11 +189,9 @@ public class SMSS_Problem {
             return sequence.get(i);
         } else {
             for (int k = i; k < j; k++) {
-                return sigRec(i,k, sequence) + sigRec(k + 1, j, sequence);
+                return sigRec(i, k, sequence) + sigRec(k + 1, j, sequence);
             }
         }
-
         return 0; // this should never be reached
     }
-
 }
