43e6624c075d3265a116236a7ab9940c96e6b593
Malte Weyrich <malte.weyrich@hotmail.de>
2024-04-27 15:28:09 +0200

    adding plot script

--- plot.py
+++ plot.py
@@ -9,5 +9,23 @@ if __name__ == "__main__":
     path = sys.argv[1]
     df = pd.read_csv(path, sep=";")
 
-    print(df.head())
+    sns.set_theme("paper")
+    sns.set_palette("colorblind")
+    sns.set_style("ticks")
+    plt.figure(figsize=(10, 6))
+
+    plt.plot(df["n"], df["smss"], label="smss")
+    plt.plot(df["n"], df["optimal"], label="optimal")
+    plt.plot(df["n"], df["divide"], label="divide")
+    plt.plot(df["n"], df["dynamic"], label="dynamic")
+    plt.plot(df["n"], df["naive"], label="naive")
+
+    plt.xlabel("n", fontsize=15)
+    plt.ylabel("time in μ", fontsize=15)
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=15)
+    plt.legend(fontsize=15)
+    sns.despine()
+
+    plt.savefig(bbox_inches="tight", fname=sys.argv[1].split(".")[0])
 
