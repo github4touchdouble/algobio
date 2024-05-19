868bb95ccdea1f299ac28d7d4b28a6e8f9cfaf56
Winston Smith <59091490+github4touchdouble@users.noreply.github.com>
2024-05-20 01:26:57 +0200

    added rounding

--- src/GRAPH/CityGraph.java
+++ src/GRAPH/CityGraph.java
@@ -4,18 +4,20 @@ import java.util.ArrayList;
 
 public class CityGraph extends Graph{
     private boolean debug = false;
+    private boolean round = false;
     public CityGraph() {
         super();
     }
-    public CityGraph(boolean debug) {
+    public CityGraph(boolean debug, boolean round) {
         super();
         this.debug = debug;
+        this.round = round;
     }
     @Override
     public void add_edge(Vertex v1, Vertex v2) {
         City c1 = (City) v1.node;
         City c2 = (City) v2.node;
-        double distance = compute_distance(c1, c2);
+        double distance = compute_distance(c1, c2, round);
         if (super.adj_list.get(v1).get(distance) == null) {
             super.adj_list.get(v1).put(distance, new ArrayList<>());
             super.adj_list.get(v1).get(distance).add(v2);
@@ -45,12 +47,16 @@ public class CityGraph extends Graph{
         }
     }
 
-    private double compute_distance(City c1, City c2){
+    private double compute_distance(City c1, City c2, boolean round){
         double lat1 = c1.lat;
         double lon1 = c1.lon;
         double lat2 = c2.lat;
         double lon2 = c2.lon;
-        return Math.sqrt(Math.pow(lat1 - lat2, 2) + Math.pow(lon1 - lon2, 2));
+        double unrounded_distance = Math.sqrt(Math.pow(lat1 - lat2, 2) + Math.pow(lon1 - lon2, 2));
+        if (!round) {
+            return unrounded_distance;
+        }
+        return (double) Math.round(unrounded_distance * 100.0) / 100.0;
     }
 
 }
--- src/GRAPH/Main.java
+++ src/GRAPH/Main.java
@@ -47,6 +47,7 @@ public class Main {
 
     public static void task1(String tsv_path) {
         List<List<String>> records = new ArrayList<>();
+
         try (BufferedReader br = new BufferedReader(new FileReader(tsv_path))) {
             String line;
             while ((line = br.readLine()) != null) {
@@ -62,12 +63,19 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        Graph g = new CityGraph();
+        Graph g = new CityGraph(false, true);
+        long add_vertex_time = 0;
         for (List<String> record : records) {
+            long s = System.currentTimeMillis();
             g.add_vertex(new Vertex<>(new City(record.get(1), Double.parseDouble(record.get(2)), Double.parseDouble(record.get(3))), Integer.parseInt(record.get(0))));
+            long e = System.currentTimeMillis();
+            add_vertex_time += e - s;
         }
+        long s = System.currentTimeMillis();
         g.compute_edges();
-        System.out.println("Read " + g.get_vertex_count() + " cities");
-        System.out.println("Generated " + g.get_edge_count() + " edges");
+        long e = System.currentTimeMillis();
+
+        System.out.println("Added " + g.get_vertex_count() + " cities into the City Graph in " + add_vertex_time + "ms");
+        System.out.println("Generated " + g.get_edge_count() + " City Graph edges in " + (e - s) + "ms");
     }
 }
