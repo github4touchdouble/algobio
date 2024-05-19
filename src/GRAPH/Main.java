1576486615af083e7e423ffcf8464038abb5a24b
Winston Smith <59091490+github4touchdouble@users.noreply.github.com>
2024-05-20 00:23:28 +0200

    tidied up

--- src/GRAPH/Main.java
+++ src/GRAPH/Main.java
@@ -7,13 +7,6 @@ public class Main {
         g.add_vertex(new Vertex<>(new City("Dettelbach (Bayern)", 49.48, 10.11), 1));
         g.add_vertex(new Vertex<>(new City("Hiroshima (Japan)", 34.23, 132.27), 2));
 
-        //Graph g = new Graph();
-        //Vertex<String> v1 = new Vertex<>("A", "A");
-        //Vertex<String> v2 = new Vertex<>("B", "B");
-        //Vertex<String> v3 = new Vertex<>("C", "C");
-        //g.add_vertex(v1);
-        //g.add_vertex(v2);
-        //g.add_vertex(v3);
         g.compute_edges();
 
         System.out.println(g);
