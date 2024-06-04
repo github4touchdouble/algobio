efa14678c4e75a6e008a90c918494a0cbab37251
Malte Weyrich <malte.weyrich@hotmail.de>
2024-06-04 16:23:04 +0200

    task 2 done

--- src/GRN/GRN.java
+++ src/GRN/GRN.java
@@ -1,16 +1,17 @@
 package GRN;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class GRN {
 
-    private HashMap<String, ArrayList<String>> tfToGenes = new HashMap<>();
-    private HashMap<String, ArrayList<Variable>> geneToTfs = new HashMap<>();
-    private HashMap<String, Integer> tfToId = new HashMap<>();
+    private final HashMap<String, ArrayList<String>> tfToGenes = new HashMap<>(); // this maps all tfs to all genes they influence,
+                                                                                  // from this we can create the clauses
+    private final HashMap<String, ArrayList<Variable>> geneToTfs = new HashMap<>(); // this maps genes to all the tfs they are influenced by
+    private final HashMap<String, Integer> tfToId = new HashMap<>(); // we use this the give each variable (tf) to a fix id
+    private final HashMap<String, String> idToTf = new HashMap<>(); // we also need this to map back to the tf names from the sap solver output
+    private final HashMap<String, Boolean> tfStates = new HashMap<>(); // here we will store the actual values for each tf
 
     public GRN (String pathToEdgeList, String pathToActiveGenes) throws IOException {
         initNetwork(pathToEdgeList, pathToActiveGenes);
@@ -20,10 +21,10 @@ public class GRN {
 
         String line;
 
-        // init clauses (by default set to false
         BufferedReader br = new BufferedReader(new FileReader(pathToEdgeList));
 
-        int tfId = 1;
+        int tfId = 1; // this it the uniq id for each TF
+        // go through network file and fill up all HashMaps
         while ((line = br.readLine()) != null) {
             if (line.charAt(0) != '#') {
                 String[] values = line.split("\t");
@@ -46,19 +47,24 @@ public class GRN {
 
                 if(!tfToId.containsKey(tf)) {
                     tfToId.put(tf, tfId);
+                    idToTf.put(String.valueOf(tfId), tf);
                     tfId++;
                 }
 
+                // init all vars with false for now
+                if(!tfStates.containsKey(tf)) {
+                    tfStates.put(tf, false);
+                }
             }
         }
         br.close();
 
         // adjust active genes in network
         br = new BufferedReader(new FileReader(pathToActiveGenes));
-        while ((line = br.readLine()) != null) {
-            String[] values = line.split("\t");
-            String activeGene = values[0];
+        String activeGene;
+        while ((activeGene = br.readLine()) != null) {
 
+            // go through all tfs (variables) of gene x (clause) and dont them (they are negated by default)
             ArrayList<Variable> tfsOfGene = geneToTfs.get(activeGene);
             if (tfsOfGene != null) {
                 for(Variable tf : tfsOfGene) {
@@ -69,20 +75,55 @@ public class GRN {
         br.close();
     }
 
-    @Override
-    public String toString() {
+    public String toSAP() {
        StringBuilder sb = new StringBuilder();
        // format header
-       sb.append("p cnf ").append(this.geneToTfs.size()).append(" ").append(tfToGenes.size()).append("\n");
+       // tfToGenes.size() == Variables, since our tfs are variables
+       // geneToTfs.size() == Clauses, since our genes represent clauses
+       sb.append("p cnf ").append(this.tfToGenes.size()).append(" ").append(geneToTfs.size()).append("\n");
 
        for (String gene: geneToTfs.keySet()) {
            for (Variable tf : geneToTfs.get(gene)) {
-               String neg = tf.isNegated() ? "-" : "";
+               String neg = tf.isNegated() ? "-" : ""; // get prefix of variable (- if negated, '' if not)
                sb.append(neg).append(tfToId.get(tf.getName())).append(" ");
            }
-           sb.append("0\n");
+           sb.append("0\n"); // end line wth 0
        }
-
        return sb.toString();
+    }
+
+    public void assignVariableValues(String pathToSolved) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(pathToSolved));
+
+        String line;
+
+        // read the last line of akmaxsap output and update values in global hasmap
+        // we need to map the id of the variable -> nameOfTf -> value
+        while((line = br.readLine()) != null){
+            if (line.startsWith("v")) {
+                String[] variableAssignments = line.split(" ");
+                for(String var: variableAssignments) {
+                    if (!var.equals("v")) {
+                        String key = var.replace("-",""); // remove - for key
+                        if(!var.startsWith("-")){
+                            // update value to true
+                            tfStates.replace(idToTf.get(key), false, true);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void getActiveTfs(){
+        // print active tfs
+        int active = 0;
+        for(String tf: tfStates.keySet()) {
+            if (tfStates.get(tf)) {
+                System.out.println(tf);
+                active++;
+            }
+        }
+        System.out.println(active + " Active TFs in GRN");
     }
 }
--- src/GRN/Main.java
+++ src/GRN/Main.java
@@ -1,23 +1,112 @@
 package GRN;
 
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.*;
+
+
+
+import static net.sourceforge.argparse4j.impl.Arguments.storeTrue;
 
 public class Main {
 
-    public static void main(String args[]) throws IOException {
-        GRN grn = new GRN("/home/malte/projects/AlgoBioI/src/GRN/regulatory-network.edgelist_test.tsv", "/home/malte/projects/AlgoBioI/src/GRN/active-genes_test.txt");
-        // GRN grn = new GRN("/home/malte/projects/AlgoBioI/src/GRN/regulatory-network.edgelist.tsv", "/home/malte/projects/AlgoBioI/src/GRN/active-genes.txt");
-        System.out.println(grn.toString());
+    public static void main(String args[]) throws IOException, ArgumentParserException, InterruptedException {
+        ArgumentParser parser = ArgumentParsers.newFor("GRN").build().defaultHelp(true).description("Argparser for Gene Regulatory Network and akmaxsat solver.");
+        parser.addArgument("-network")
+                .help("Network File")
+                .required(false);
+        parser.addArgument("-actives")
+                .help("List of active genes in network")
+                .required(false);
+        parser.addArgument("-cnf")
+                .help("Path to dimacs file")
+                .required(false);
+        parser.addArgument("-sol")
+                .help("Akmaxsat solver output file")
+                .required(false);
+        parser.addArgument("-all")
+                .help("Perform all tasks and return only active TFs")
+                .action(storeTrue())
+                .required(false);
 
-        wirteSAP("sap.dimacs.txt", grn.toString());
+        // load all args
+        Namespace ns = parser.parseArgs(args);
+        String pathToNetwork = ns.getString("network");
+        String pathToActiveGenes = ns.getString("actives");
+        String pathToDimacs = ns.getString("cnf");
+        String pathToSolvedDimacs = ns.getString("sol");
+        boolean all = ns.getBoolean("all");
+
+        // exec all
+        if (all) {
+            String defaultSAPOutPath = "out.sap.dimacs.txt";
+            String solvedSAPPath = "SOLVED.txt";
+            GRN grn = new GRN(pathToNetwork, pathToActiveGenes);
+            writeSAP(defaultSAPOutPath, grn.toSAP());
+            execAkmaxsat(defaultSAPOutPath, "SOLVED.txt");
+            grn.assignVariableValues(solvedSAPPath);
+            grn.getActiveTfs();
+        }
+        else {
+            // task 1
+            if (pathToNetwork != null && pathToActiveGenes != null) {
+                GRN grn = new GRN(pathToNetwork, pathToActiveGenes);
+
+                if (pathToDimacs == null && pathToSolvedDimacs == null) {
+                    System.out.println(grn.toSAP());
+                }
+
+                // task 3
+                else if (pathToSolvedDimacs != null) {
+                    grn.assignVariableValues(pathToSolvedDimacs);
+                    grn.getActiveTfs();
+                }
+            }
+
+            // task 2
+            else if (pathToDimacs != null) {
+                execAkmaxsat(pathToDimacs, "SOLVED.txt");
+            }
+        }
     }
 
 
-    public static void wirteSAP(String path, String content) throws IOException {
+    public static void writeSAP(String path, String content) throws IOException {
         BufferedWriter bw = new BufferedWriter(new FileWriter(path));
         bw.write(content);
         bw.close();
+    }
+
+    public static void execAkmaxsat(String pathToDimacs, String outPath) throws IOException, InterruptedException {
+
+        String[] command =  {"./akmaxsat", pathToDimacs};
+        ProcessBuilder pb = new ProcessBuilder(command).redirectErrorStream(true);
+        Process process = pb.start();
+        // Capture the output and write to SOLVED.txt
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outPath));
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            writer.write(line);
+            writer.newLine();
+        }
+
+        reader.close();
+        writer.close();
+        int exitCode = process.waitFor();
+
+        if (exitCode == 0) {
+            System.out.println("Akmaxsat executed successfully.");
+
+        } else {
+            System.err.println("Akmaxsat failed with exit code: " + exitCode);
+        }
     }
 }
